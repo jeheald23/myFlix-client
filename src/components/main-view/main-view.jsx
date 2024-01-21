a92020732854e503dd6378082f717185167e11ab
jeheald23 <jeheald23@gmail.com>
2024-01-21 17:29:08 +0100

    resolved conflicts

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-import { useState, useEffect } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
-
-=======
 import React, { useState, useEffect, useCallback } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
@@ -11,63 +5,9 @@ import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
->>>>>>> Stashed changes
 
 export const MainView = () => {
 
-<<<<<<< Updated upstream
-  const [movies, setMovies] = useState([]);
-
-const [selectedMovie, setSelectedMovie] = useState(null);
-
-useEffect(() => {
-  fetch("https://myflixapp-api-3e4d3ace1043.herokuapp.com/movies")
-    .then((response) => response.json())
-    .then((data) => {
-        const moviesFromApi = data.map((movie) => {
-          return {
-          image: movie.image,
-          title: movie.title,
-          releaseYear: movie.releaseYear,
-          description: movie.description,
-          genre: {
-            name: movie.genre.name,
-            description: movie.genre.description},
-          director: {
-            name: movie.director.name,
-            bio: movie.director.bio},
-          featured:movie.featured,
-          actors: movie.actors
-          };
-        });
-
-      setMovies(moviesFromApi);
-    });
-}, []);
-
-  if (selectedMovie) {
-    return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
-
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
-
-  return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-    </div>
-=======
   const fetchMovies = useCallback(async () => {
     try {
       if (token) {
@@ -147,6 +87,5 @@ useEffect(() => {
         </>
       )}
     </Row>
->>>>>>> Stashed changes
   );
 };
