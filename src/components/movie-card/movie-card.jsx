90f2a21badf4292e7385d0cc74a8ec90beb8900d
jeheald23 <jeheald23@gmail.com>
2024-01-22 22:31:39 +0100

    fixed errors

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,19 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
-    );
-  };
+  return (
+    <Card className="h-100" onClick={() => onMovieClick(movie)}>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director.name}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
 
   MovieCard.propTypes = {
     movie: PropTypes.shape({
