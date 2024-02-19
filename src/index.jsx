f86c52b10416fbe33e46ae134918b0be0b3e7f6c
jeheald23 <jeheald23@gmail.com>
2024-02-19 18:00:06 +0100

    fixed error

--- src/index.jsx
+++ src/index.jsx
@@ -13,20 +13,6 @@ const App = () => {
   );
 };
 
-const cors = require('cors');
-let allowedOrigins = ["https://jeheald23myflix.netlify.app/"];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 const container = document.querySelector("#root");
 const root = createRoot(container);
 root.render(<App />);
